c3e140388a550569d44acf754e81450555ad5a6e
jschrom311 <jschrom311@yahoo.com>
2019-07-05 19:23:08 -0500

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,6 @@
+#Simple Tic Tac Toe game
+#Two player
+#Board prints each time a player makes a move
+#Game accepts input of player and places symbol to board
+
+from IPython.display import clear_output
